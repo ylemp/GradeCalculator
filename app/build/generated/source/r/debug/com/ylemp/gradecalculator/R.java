8e529d71dffcdc34ed0e11a141f3c1cd5d396a22
Chris burns <chrisotph.burns@gmail.com>
2014-08-20 00:44:41 -0400

    Add scores for each course
    and debugging methods

--- app/build/generated/source/r/debug/com/ylemp/gradecalculator/R.java
+++ app/build/generated/source/r/debug/com/ylemp/gradecalculator/R.java
@@ -21,20 +21,21 @@ public final class R {
         public static final int plus=0x7f020003;
     }
     public static final class id {
-        public static final int action_create=0x7f08000c;
-        public static final int action_delete=0x7f08000d;
-        public static final int action_settings=0x7f08000e;
+        public static final int action_create=0x7f08000d;
+        public static final int action_delete=0x7f08000e;
+        public static final int action_settings=0x7f08000f;
         public static final int class_name=0x7f080006;
         public static final int grade_name=0x7f080001;
-        public static final int progressBar=0x7f08000a;
+        public static final int grade_per=0x7f080008;
+        public static final int progressBar=0x7f080009;
         public static final int score=0x7f080004;
-        public static final int score_per=0x7f080009;
+        public static final int score_per=0x7f08000b;
         public static final int submit=0x7f080007;
         public static final int submit2=0x7f080003;
         public static final int submit3=0x7f080005;
         public static final int text1=0x7f080000;
-        public static final int text2=0x7f080008;
-        public static final int text3=0x7f08000b;
+        public static final int text2=0x7f08000a;
+        public static final int text3=0x7f08000c;
         public static final int weight=0x7f080002;
     }
     public static final class layout {
